57a820e8ec99ba2374cb0fc2b5ad4b575a29101c
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-08 13:04:58 +0300

    added section with background and heading text

--- src/Apartments.jsx
+++ src/Apartments.jsx
@@ -5,8 +5,10 @@ function Apartments() {
     <section>
       <div className="blurred-background">
         <div className="text-content">
-          <h1>Your Blurred Background Component</h1>
-          <p>This is some text on the blurred background.</p>
+          <h1>Kingdom Apartments</h1>
+          <p>Your ideal home.</p>
+          <p>Location: Ngong road</p>
+
         </div>
       </div>
     </section>
