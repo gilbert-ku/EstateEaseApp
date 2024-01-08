ff03b23b8715da5de07d7f3b633663953f87c145
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-08 18:38:47 +0300

    styled the apartment card

--- src/Housecard.jsx
+++ src/Housecard.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 function Housecard() {
   return (
     <>
+    <div id='housecard'>
     <div id='housecard-image'>
         <img src='https://www.truoba.com/wp-content/uploads/2022/09/Truoba-Mini-1-bedroom-house-plans-1200x800.jpg' alt=''></img>
     </div>
-    <div>
+    <div id='title'>
         <h1>1-Bedroom Apartments</h1>
         <p>Perfect for individuals or couples, our 1-bedroom apartments offer simplicity and comfort. 
             Enjoy a well-equipped kitchen, cozy living space, and convenient amenities</p>
-            <div id='desription'>
-                <h2>Monthly Rent: Ksh 18000</h2>
+            <div id='description'>
+                <h2>Monthly Rent: <span>Ksh 18000</span></h2>
                 <h2>Vacant Apartment</h2>
             </div>
             <div id='buttons'>
@@ -19,6 +20,8 @@ function Housecard() {
                 <button>Rent</button>
             </div>
     </div>
+    </div>
+
     </>
   )
 }
