9fc7381385a80b2cc59b337ee74c0d6fe731bbd7
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-08 18:16:10 +0300

    defined card structure

---
+++ src/Housecard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Housecard() {
+  return (
+    <>
+    <div id='housecard-image'>
+        <img src='https://www.truoba.com/wp-content/uploads/2022/09/Truoba-Mini-1-bedroom-house-plans-1200x800.jpg' alt=''></img>
+    </div>
+    <div>
+        <h1>1-Bedroom Apartments</h1>
+        <p>Perfect for individuals or couples, our 1-bedroom apartments offer simplicity and comfort. 
+            Enjoy a well-equipped kitchen, cozy living space, and convenient amenities</p>
+            <div id='desription'>
+                <h2>Monthly Rent: Ksh 18000</h2>
+                <h2>Vacant Apartment</h2>
+            </div>
+            <div id='buttons'>
+                <button>Book a visit</button>
+                <button>Rent</button>
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Housecard
