9822035660bed46f0923035655f7e3817bc9be60
gilbert-ku <gilbert45ku@gmail.com>
2024-01-09 01:57:34 +0300

    add search box

--- src/components/home/Homepg.jsx
+++ src/components/home/Homepg.jsx
@@ -4,8 +4,23 @@ const Homepg = () => {
   return (
     <>
       <section className='landing--page '>
-        <div className='container-fuild bg-primary landing--image'>
-          <p className='text-center '>Homepg</p>
+        <div className='container-fuild d-flex align-items-center justify-content-center landing--image'>
+        
+            <div className='warning search--box'>
+              <p className='mt-1 p-1 text-center fs-1 fw-bold ' style={{color: "#9C5412"}}>
+                EstateEase
+              </p>
+              <p className='fs-3 text-center text-wrap'>
+                Elevate Your Lifestyle: Find,        
+              </p>
+              <p className='fs-4 text-center' style={{marginTop: "-20px"}}>
+              Rent, and Live in Style.
+              </p>
+              
+            </div>
+          
+
+          
         </div>
         
       </section>
