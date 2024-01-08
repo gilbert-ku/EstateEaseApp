68ea2ebfb98167573fdba3bd596435c55561d2aa
gilbert-ku <gilbert45ku@gmail.com>
2024-01-09 02:31:46 +0300

    add search bar

--- src/components/home/Homepg.jsx
+++ src/components/home/Homepg.jsx
@@ -10,12 +10,16 @@ const Homepg = () => {
               <p className='mt-1 p-1 text-center fs-1 fw-bold ' style={{color: "#9C5412"}}>
                 EstateEase
               </p>
-              <p className='fs-3 text-center text-wrap'>
+              <p className='fs-3 text-center font-monospace text-wrap fw-bold'>
                 Elevate Your Lifestyle: Find,        
               </p>
-              <p className='fs-4 text-center' style={{marginTop: "-20px"}}>
+              <p className='fs-4 text-center font-monospace fw-bold' style={{marginTop: "-20px"}}>
               Rent, and Live in Style.
               </p>
+              <button className='ms-5 fs-5 fw-bold text-white rent--btn' style={{width:"10rem"}}>Rent</button>
+              <div className='bg-white mb-3 mx-5 search--bar' style={{minHeight: "5rem"}}>
+
+              </div>
               
             </div>
           
