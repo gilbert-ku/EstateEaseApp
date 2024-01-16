004cf21d8326f3c8923d14771d18b6b8586162a0
gilbert-ku <gilbert45ku@gmail.com>
2024-01-17 00:12:08 +0300

    add footer quick links

--- src/components/navbar/Footer.jsx
+++ src/components/navbar/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
         <div className='container-fluid '>
           <div className='footer--div mx-auto rounded-4'>
-            <div>
+            <div className=''>
               <p className='fs-1 ms-5 text-white fw-bold'>EstateEase</p>
               <p className='fs-4 ms-5 text-white ' >Subscribe for EstateEase updates </p>
               <div className='m-5 mt-2'>
@@ -17,6 +17,29 @@ const Footer = () => {
                   </div>
                 </form>
               </div>
+            </div>
+
+            <div className='quiz-link'>
+              <p className='fs-5'>Quick Links</p>
+              <ul>
+                <li>About us</li>
+                <li>contact Us</li>
+                <li>Properties</li>
+                <li>Find Help</li>
+              </ul>
+            </div>
+
+            <div>
+              <p className='fs-5'>Useful Link</p>
+
+              <ul>
+                <li>Private Policy</li>
+                <li>User Agreement</li>
+                <li>Frequently Asked Question</li>
+                <li>
+                  <button className='btn '>Find Movers</button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
