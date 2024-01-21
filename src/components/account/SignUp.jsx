4f13c26bab434d914fd31cf6e7ff72929de093bb
gilbert-ku <gilbert45ku@gmail.com>
2024-01-21 23:52:19 +0300

    add signup icon

--- src/components/account/SignUp.jsx
+++ src/components/account/SignUp.jsx
@@ -8,18 +8,18 @@ const SignUp = () => {
     <div className='container'>
       <div className='sign--form'>
         <p className='fs-1'>Welcome To <span className='fs-1 ' style={{color: "#9C5412"}}>EstateEase</span></p>
-        <p>Please Enter your credential</p>
+        <p>Please Crreate account</p>
           <form>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Full Name</label>
-              <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='John Doe' />
+              <label  className="form-label">Full Name</label>
+              <input type="text" className="form-control" id="exampleInputName" aria-describedby="emailHelp" placeholder='John Doe  ' />
             </div>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
+              <label  className="form-label">Email address</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='example@gmail.com' />
             </div>
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">Password</label>
+              <label  className="form-label">Password</label>
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder='••••••••'/>
             </div>
             {/* <div className="mb-3 form-check">
@@ -29,7 +29,7 @@ const SignUp = () => {
             <button type="submit" className="btn btn-primary w-100" style={{color: "white", backgroundColor: '#9C5412'}}>Sign Up</button>
 
           </form>
-          <div className='d-flex justify-content-center align-items-center'>
+          <div className='d-flex justify-content-center align-items-center my-1'>
             <hr className='w-50 my-auto border-2'/> 
             <p className='mx-4 my-auto'>OR </p>  
             <hr className='w-50 my-auto border-2'/>
