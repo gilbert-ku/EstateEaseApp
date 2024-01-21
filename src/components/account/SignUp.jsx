c8b3f790a54db2b9c53eb679bfb976c08c5e7fa8
gilbert-ku <gilbert45ku@gmail.com>
2024-01-22 00:21:33 +0300

    add bootstrap slides

--- src/components/account/SignUp.jsx
+++ src/components/account/SignUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { FaGoogle } from "react-icons/fa";
+import ngongrd from '/ngong-dr-apt.jpg';
+
 
 const SignUp = () => {
   return (
     <>
 
-    <div className='container'>
+    <div className='container d-flex'>
       <div className='sign--form'>
         <p className='fs-1'>Welcome To <span className='fs-1 ' style={{color: "#9C5412"}}>EstateEase</span></p>
         <p>Please Crreate account</p>
@@ -39,6 +41,46 @@ const SignUp = () => {
            <FaGoogle className='mx-2 text-center'/>
             Sign Up With Gooogle
           </button>
+      </div>
+
+
+      <div id="carouselExampleCaptions" className="carousel slide boot--slides" data-bs-ride="carousel" >
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={ngongrd} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={ngongrd} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={ngongrd} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
     </>
