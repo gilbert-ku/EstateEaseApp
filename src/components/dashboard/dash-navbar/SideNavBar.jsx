729cb4eec6e29e071b0d181ce5acd9c225508710
gilbert-ku <gilbert45ku@gmail.com>
2024-01-26 00:28:11 +0300

    side navbar

--- src/components/dashboard/dash-navbar/SideNavBar.jsx
+++ src/components/dashboard/dash-navbar/SideNavBar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { MdOutlineDashboardCustomize, MdOutlineReviews } from "react-icons/md";
+import { BsHouses } from "react-icons/bs";
+import { IoReceiptOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const SideNavBar = () => {
   return (
 
-   
-    <div>
-        <h1></h1>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <span>
+            <MdOutlineDashboardCustomize />
+            </span>
+            Dashboard
+          </li>
+          <li>
+            <span><BsHouses /></span>
+            Property
+          </li>
+          <li>
+            <span><IoReceiptOutline /></span>
+            Receipt
+          </li>
+          <li>
+            <span><AiOutlineMessage /></span>
+            Contact Us
+          </li>
+          <li>
+            <span><MdOutlineReviews /></span>
+            Reviews
+          </li>
+        </ul>
+      </nav>
+    </>
 
   )
 }
