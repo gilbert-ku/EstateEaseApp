8c6afffb2f5fafe744f84b7deec7e212cc8d501f
gilbert-ku <gilbert45ku@gmail.com>
2024-01-26 01:38:39 +0300

    update sidebar nav style

--- src/components/dashboard/dash-navbar/SideNavBar.jsx
+++ src/components/dashboard/dash-navbar/SideNavBar.jsx
@@ -1,65 +1,61 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { MdOutlineDashboardCustomize, MdOutlineReviews } from "react-icons/md";
-import { BsHouses } from "react-icons/bs";
-import { IoReceiptOutline } from "react-icons/io5";
-import { AiOutlineMessage } from "react-icons/ai";
+import { MdOutlineDashboardCustomize, MdOutlineReviews } from 'react-icons/md';
+import { BsHouses } from 'react-icons/bs';
+import { IoReceiptOutline } from 'react-icons/io5';
+import { AiOutlineMessage } from 'react-icons/ai';
 
 const SideNavBar = () => {
   return (
-
-    <>
-      <nav className="navbar ">
-        <ul className="navbar-nav">
-
-          <li className="nav-item ">
-            <Link to="/Dashboard" className="nav-link text-white">
-              <span className="me-1">
-                <MdOutlineDashboardCustomize />
-              </span>
-              Dashboard
-            </Link>
-          </li>
-
-          <li className="nav-item"> 
-            <Link to="/property" className="nav-link text-white">
-              <span className="me-1">
-                <BsHouses />
-              </span>
-              Property
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="/receipt" className="nav-link text-white">
-              <span className="me-1">
-                <IoReceiptOutline />
-              </span>
-              Receipt
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="/contactUs" className="nav-link text-white">
-              <span className="me-1">
-                <AiOutlineMessage />
-              </span>
-              Contact Us
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/revies" className="nav-link text-white">
-            <span className="me-1">
-              <MdOutlineReviews />
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/Dashboard" className="nav-link text-white">
+            <span className="icon-name">
+              <MdOutlineDashboardCustomize className='me-1'/>
+              <span className="d-none d-md-inline">Dashboard</span>
             </span>
-              Reviews
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+          </Link>
+        </li>
 
-  )
-}
+        <li className="nav-item">
+          <Link to="/property" className="nav-link text-white">
+            <span className="icon-name ">
+              <BsHouses className='me-1'/>
+              <span className="d-none d-md-inline">Property</span>
+            </span>
+          </Link>
+        </li>
 
-export default SideNavBar
+        <li className="nav-item">
+          <Link to="/receipt" className="nav-link text-white">
+            <span className="icon-name">
+              <IoReceiptOutline className='me-1'/>
+              <span className="d-none d-md-inline">Receipt</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/contactUs" className="nav-link text-white">
+            <span className="icon-name">
+              <AiOutlineMessage className='me-1'/>
+              <span className="d-none d-md-inline">Contact Us</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/reviews" className="nav-link text-white">
+            <span className="icon-name">
+              <MdOutlineReviews className='me-1'/>
+              <span className="d-none d-md-inline">Reviews</span>
+            </span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavBar;
