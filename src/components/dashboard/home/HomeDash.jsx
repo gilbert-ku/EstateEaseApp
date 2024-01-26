83f0489075c59bc9c3b231fb5945abb53c5c8476
gilbert-ku <gilbert45ku@gmail.com>
2024-01-26 13:51:09 +0300

    remove carousel and add slide

--- src/components/dashboard/home/HomeDash.jsx
+++ src/components/dashboard/home/HomeDash.jsx
@@ -1,82 +1,44 @@
 import React from 'react'
-import birthroom from '/birthroom.jpeg';
-import seatingone from '/seatingroom .jpeg';
-import seatingtwo from '/seatingroom2 .jpeg';
-import kitchen from '/Kitchen.jpeg';
 import SideNavBar from '../dash-navbar/SideNavBar';
+import { Zoom } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 
 const homeDash = () => {
+  const images = [
+    "/birthroom.jpeg",
+    "/Kitchen.jpeg",
+    "/seatingroom2 .jpeg",
+    "/seatingroom .jpeg",
+    
+];
   return (
     <>
-    <section className='fluid  d-flex' >
-    <div className=' px-3 px-md-5 ' style={{backgroundColor: "#9C5412", width: ""}}>
-      <SideNavBar />
-    </div>
-    <div className="mx-2 mx-md-5 d-md-flex">
-      <div className=" d-md-block">
-            <div
-              id="carouselExampleCaptions"
-              className="carousel slide  dashboard-carousel"
-              data-bs-ride="carousel"
-              // style={{width: "30vw"}}
-            >
-              <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              </div>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img src={birthroom} className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block text-black">
-                    <p className='fs-1 text-white fw-bold'>Apartment</p>
-                    <p>Some representative placeholder content for the first slide.</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img src={kitchen} className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block text-black">
-                    <p className='fs-1 text-white fw-bold'>Bungalow</p>
-                    <p>Some representative placeholder content for the second slide.</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img src={seatingone} className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block text-black">
-                    <p className='fs-1 text-white fw-bold'>Mansion</p>
-                    <p>Some representative placeholder content for the third slide.</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img src={seatingtwo} className="d-block w-100" alt="..." />
-                  <div className="carousel-caption d-none d-md-block text-black">
-                    <p className='fs-1 text-white fw-bold'>Mansion</p>
-                    <p>Some representative placeholder content for the third slide.</p>
-                  </div>
-                </div>
-              </div>
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-              </button>
-            </div>
+    <section className='fluid d-flex ' >
+      <div className='px-3' style={{backgroundColor: "#9C5412"}}>
+        <SideNavBar />
       </div>
+      <div className=" d-sm-flex">
+        {/*  slide*/}
 
-      <div className='d-flex justify-center align-canter m-auto'>
-        <div className=''>
-        {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis illum voluptatibus corrupti, consequatur earum dolorum vitae nulla? Aspernatur laboriosam earum tenetur eveniet non, harum assumenda quos, quo rerum necessitatibus quaerat.</p> */}
-        <button className="btn btn-secondary color-white">Rent our Specious houses to day</button>
+        <div className="responsive-container mx-1">
+          <Zoom scale={0.7} indicators={true}>
+            {images.map((each, index) => (
+              <div key={index} className="responsive-item mx-1">
+                <img className="img-fluid " alt="Slide Image" src={each} />
+              </div>
+            ))}
+          </Zoom>
+        </div>
+
+        <div className='w-100 w-sm-50 text-center'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi temporibus incidunt porro obcaecati similique, minima, maiores corrupti perspiciatis neque quaerat error laudantium omnis, nisi nemo amet sequi dolor molestiae.</p>
+          <button className="btn btn-secondary color-white">Rent our Specious houses to day</button>
         </div>
       </div>
-    </div>
     </section>
     </>
   )
 }
 
-export default homeDash
+export default homeDash;  
