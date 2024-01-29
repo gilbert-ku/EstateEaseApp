b9fec7d4f3c43fc9d86e1bc8103c8337509627a9
gilbert-ku <gilbert45ku@gmail.com>
2024-01-29 23:54:44 +0300

    add contact form

--- src/components/dashboard/home/RentForm.jsx
+++ src/components/dashboard/home/RentForm.jsx
@@ -14,7 +14,7 @@ const RentForm = () => {
       <section className='container-fluid' style={{ height: "100vh" }}>
         <NavbarDash />
         <div className='container'>
-           <p className='fs-6 fs-md-5 mx-5'>
+           <p className='fs-6 fs-md-5 mx-md-5'>
            Welcome to EstateEase! Elevate your living with sophisticated homes, secure payments, instant receipts, and top-notch safety. Experience the pinnacle of elegance and convenience. Fill the rent form today for a seamless journey into luxury living.            </p> 
         </div>
         
--- src/components/dashboard/info/Contact.jsx
+++ src/components/dashboard/info/Contact.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
+    <div className='contact---us bg-warning text-center'>
+      <div className=''>
+        <h3 className='fs-3'style={{color: "#9C5412"}}>EstateEase</h3>
+        <p className='fs-1 fw-bold text-light'>Contact Us</p>
+      </div>
       
+    </div>
+
+     <div className="container">
+        
+        <div className='my-5'>
+          
+        <div className="mb-3">
+            <label htmlFor="nameFormControlInput1" className="form-label">Full Name</label>
+            <input type="email" className="form-control" id="nameFormControlInput1" placeholder="John Doe" required/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="emailFormControlInput1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="emailFormControlInput1" placeholder="johndoe@example.com" required/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="messangeFormControlTextarea1" className="form-label">Message</label>
+            <textarea className="form-control" id="messangeFormControlTextarea1" required rows="3"></textarea>
+          </div>
+
+          <button className="btn text-white w-100" style={{backgroundColor: "#9C5412"}}>Submit</button>
+        </div>
+     </div>
+      
+
+
     </>
   )
 }
