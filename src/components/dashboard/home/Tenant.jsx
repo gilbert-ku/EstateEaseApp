3c19a53f59f5a0dd8cc3456811e5148d5e1142b7
gilbert-ku <gilbert45ku@gmail.com>
2024-02-01 23:51:29 +0300

    update profile

--- src/components/dashboard/home/Tenant.jsx
+++ src/components/dashboard/home/Tenant.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { BsHouseAdd } from "react-icons/bs";
+import { IoNotificationsSharp } from "react-icons/io5";
 import  seenbungalow from "/seenbungalow.jpg"
 import birthroom from "/birthroom.jpeg"
 import kitchen from "/Kitchen.jpeg"
@@ -17,7 +19,7 @@ const Tenant = () => {
         <section className="container " style={{height: "100vh", width:"100vw"}}>
             <h1>Dashbord</h1>
             {/* new applicant */}
-            <div className='d-md-flex justify-content-between border border-2 p-2 rounded-3 mt-5'>
+            <div className='d-md-flex justify-content-between border border-2 p-2 rounded-3 mt-1 mt-md-2'>
 
                 <div className='d-flex '>
                     <div className=' p-1 p-md-2 fs-3 rounded-3 my-auto' style={{backgroundColor: "#9C5412"}}>
@@ -43,7 +45,7 @@ const Tenant = () => {
                 {/* ******************************************* */}
                 <div className=''>
                     {/* payments */}
-                    <div className=' border border-2 p-2 rounded-3 mt-5'>
+                    <div className=' border border-2 p-2 rounded-3 mt-1 mt-md-2'>
                         <p className='ms-4 ms-md-2'> <span className='fw-bold'>Schedule Payment</span> <span className='pay--schedul p-1 rounded-3'>DUE IN 7 Days</span></p>
 
                         <hr  className=''/> 
@@ -72,7 +74,7 @@ const Tenant = () => {
                     </div>
 
                 {/* property */}
-                <div className=' border border-2 p-2 rounded-3 mt-5'>
+                <div className=' border border-2 p-2 rounded-3 mt-1 mt-md-2'>
                     <p className='fw-bold'>Properties</p>
                     <p>From the most recent properties you see</p>
                     <div className='property--seen d-md-flex justify-content-between'>
@@ -96,36 +98,34 @@ const Tenant = () => {
                                     </div>
                                     <div className="carousel-inner">
                                     
-                                    <div className="carousel-item">
-                                        <img src={sitting} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
-                                        <div className="carousel-caption d-none d-md-block text-black">
-                                        <p className='fs-3 text-white fw-bold'>Sitting Room</p>
-                                        <p>Some representative placeholder content for the second slide.</p>
+                                        <div className="carousel-item">
+                                            <img src={sitting} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
+                                            <div className="carousel-caption d-none d-md-block text-black">
+                                            <p className='fs-3 text-white fw-bold'>Sitting Room</p>
+                                            <p>Some representative placeholder content for the second slide.</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={sitting2} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
-                                        <div className="carousel-caption d-none d-md-block text-black">
-                                        <p className='fs-3 text-white fw-bold'>Sitting Room</p>
-                                        <p>Some representative placeholder content for the second slide.</p>
+                                        <div className="carousel-item">
+                                            <img src={sitting2} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
+                                            <div className="carousel-caption d-none d-md-block text-black">
+                                            <p className='fs-3 text-white fw-bold'>Sitting Room</p>
+                                            <p>Some representative placeholder content for the second slide.</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={kitchen} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
-                                        <div className="carousel-caption d-none d-md-block text-black">
-                                        <p className='fs-3 text-white fw-bold'>Kitchen</p>
-                                        <p>Some representative placeholder content for the third slide.</p>
+                                        <div className="carousel-item">
+                                            <img src={kitchen} className="d-block w-100" alt="..." style={{height: "25rem"}}/>
+                                            <div className="carousel-caption d-none d-md-block text-black">
+                                            <p className='fs-3 text-white fw-bold'>Kitchen</p>
+                                            <p>Some representative placeholder content for the third slide.</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="carousel-item active">
-                                        <img src={birthroom} className="d-block w-100" alt="..."  style={{height: "25rem"}}/>
-                                        <div className="carousel-caption d-none d-md-block text-black">
-                                        <p className='fs-3 text-white fw-bold'>Birthroom</p>
-                                        <p>Some representative placeholder content for the first slide.</p>
+                                        <div className="carousel-item active">
+                                            <img src={birthroom} className="d-block w-100" alt="..."  style={{height: "25rem"}}/>
+                                            <div className="carousel-caption d-none d-md-block text-black">
+                                            <p className='fs-3 text-white fw-bold'>Birthroom</p>
+                                            <p>Some representative placeholder content for the first slide.</p>
+                                            </div>
                                         </div>
-                                    </div>
-
-
                                     </div>
                                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -142,7 +142,7 @@ const Tenant = () => {
                 </div>
                 </div>
 
-                <div className='mt-5 border border-2 p-2 rounded-3 mx-2'>
+                <div className='mt-1 mt-md-2 border border-2 p-2 rounded-3 ms-2'>
                     <p className='ms-3 fw-bold'>My Profile</p>
                     <div className='d-flex justify-content-center align-item-center'>
                         <div style={{height: "6rem"}}>
@@ -154,59 +154,79 @@ const Tenant = () => {
                     </div>
                     <p className='my-2 text-center'>Gilbert Daddy Kutoto</p>
 
+
+                    {/* table */}
                     <div>
+                            <p className='fw-bold '
+                            style={{color: "#9C5412"}}>Overview</p>
+                            <table className="table table-bordered">
                         
-                    <table class="table table-bordered">
-                    <p className='fw-bold '
-                    style={{color: "#9C5412"}}>Overview</p>
-                        <tbody>
-                            <tr>
-                            {/* <th scope="row">1</th> */}
-                            <th scope="col">CATEGORY</th>
-                            <td>Bungalow</td>
+                            <tbody>
+                                <tr>
+                                {/* <th scope="row">1</th> */}
+                                <th scope="col">CATEGORY</th>
+                                <td>Bungalow</td>
+                                
+                                </tr>
+                                <tr>
+                                {/* <th scope="row">1</th> */}
+                                <th scope="col">HOUSE NO</th>
+                                <td>AP 17</td>
+                                </tr>
+
+                                <tr>
+                                {/* <th scope="row">1</th> */}
+                                <th scope="col">BEDROOM NO</th>
+                                <td>3</td>
+                                </tr>
                             
-                            </tr>
-                            <tr>
-                            {/* <th scope="row">1</th> */}
-                            <th scope="col">HOUSE NO</th>
-                            <td>AP 17</td>
-                            </tr>
+                                <tr>
+                                {/* <th scope="row">3</th> */}
+                                <th scope="col">PAID DATE</th>
+                                {/* <td colspan="2">Larry the Bird</td> */}
+                                <td>01/01/1014</td>
+                                </tr>
 
-                            <tr>
-                            {/* <th scope="row">1</th> */}
-                            <th scope="col">BEDROOM NO</th>
-                            <td>3</td>
-                            </tr>
-                        
-                            <tr>
-                            {/* <th scope="row">3</th> */}
-                            <th scope="col">PAID DATE</th>
-                            {/* <td colspan="2">Larry the Bird</td> */}
-                            <td>01/01/1014</td>
-                            </tr>
+                                <tr>
+                                {/* <th scope="row">3</th> */}
+                                <th scope="col">DUE DATE</th>
+                                {/* <td colspan="2">Larry the Bird</td> */}
+                                <td>01/01/2024</td>
+                                </tr>
 
-                            <tr>
-                            {/* <th scope="row">3</th> */}
-                            <th scope="col">DUE DATE</th>
-                            {/* <td colspan="2">Larry the Bird</td> */}
-                            <td>01/01/2024</td>
-                            </tr>
+                                <tr>
+                                {/* <th scope="row">3</th> */}
+                                <th scope="col">AMOUNT</th>
+                                {/* <td colspan="2">Larry the Bird</td> */}
+                                <td>Ksh 30 000</td>
+                                </tr>
 
-                            <tr>
-                            {/* <th scope="row">3</th> */}
-                            <th scope="col">AMOUNT</th>
-                            {/* <td colspan="2">Larry the Bird</td> */}
-                            <td>Ksh 30 000</td>
-                            </tr>
-
-                            <tr>
-                            {/* <th scope="row">3</th> */}
-                            <th scope="col">STATUS</th>
-                            {/* <td colspan="2">Larry the Bird</td> */}
-                            <td>Paid</td>
-                            </tr>
-                        </tbody>
+                                <tr>
+                                {/* <th scope="row">3</th> */}
+                                <th scope="col">STATUS</th>
+                                {/* <td colspan="2">Larry the Bird</td> */}
+                                <td>Paid</td>
+                                </tr>
+                            </tbody>
                         </table>
+                    </div>
+
+                    <div>   
+                        <p className='fw-bold'>Recent Activities</p>
+                        <Link to="/dashboard/notification" className="nav-link " >
+                        <p>
+                            <span>
+                                <IoNotificationsSharp className='p-1 text-white rounded-circle me-2 fs-5' style={{backgroundColor: "#9C5412"}}/>
+                                Payment Reminder
+                            </span>
+                        </p></Link>
+                        <p className='font-monospace'>The rent  payment of Ksh 30,000 is due next week.</p>
+
+                        <div className='d-flex justify-content-between'>
+                            <p><span className='pay--schedul p-1 rounded-3'>DUE IN 7 Days</span></p>
+                            <p>01/02/2024 12:00</p>
+                        </div>
+                        
                     </div>
                     
                 </div>
